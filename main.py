d8b2bd7a630be856219e9c28d74039b824fe8c41
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-05-10 14:52:47 +0300

    2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 from functions import year_declension
+import pandas
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -17,6 +18,13 @@ rendered_page = template.render(
     age = age_company,
     year_declension = year_declension(age_company)
 )
+
+excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='wines', usecols=['Название', 'Сорт', 'Цена', 'Картинка'])
+print(excel_data_df.to_dict)
+
+wines = [
+    
+]
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
