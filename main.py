6a3f067e12e51a8c1f44edcbd45667d21056d838
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-05-10 17:25:05 +0300

    4

--- main.py
+++ main.py
@@ -14,14 +14,21 @@ template = env.get_template('template.html')
 today = datetime.date.today()
 age_company = today.year - 1920
 
-# rendered_page = template.render(
-#     age = age_company,
-#     year_declension = year_declension(age_company)
-# )
+wines = pandas.read_excel('wine.xlsx', sheet_name='wines', usecols=['Название', 'Сорт', 'Цена', 'Картинка'])
+wines = wines.to_dict(orient='record')
 
-excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='wines', usecols=['Название', 'Сорт', 'Цена', 'Картинка'])
+for wine in wines:
+    wine['title'] = wine['Название'] 
+    del wine['Название']
+    wine['grade'] = wine['Сорт']
+    del wine['Сорт']
+    wine['price'] = wine['Цена']
+    del wine['Цена']
+    wine['image'] = 'images/' + wine['Картинка']
+    del wine['Картинка']
 
 rendered_page = template.render(wines=wines, age = age_company, year_declension = year_declension(age_company) )
+print(wines)
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
 
