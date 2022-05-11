5da663d561d791dde4c8f5b4d78b8385898d82aa
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-05-11 16:05:03 +0300

    5

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 from functions import year_declension
 import pandas
+from pprint import pprint
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -14,23 +15,36 @@ template = env.get_template('template.html')
 today = datetime.date.today()
 age_company = today.year - 1920
 
-wines = pandas.read_excel('wine.xlsx', sheet_name='wines', usecols=['Название', 'Сорт', 'Цена', 'Картинка'])
-wines = wines.to_dict(orient='record')
+products = pandas.read_excel('wine2.xlsx', sheet_name='Лист1', usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка'], na_values='nan', keep_default_na=False)
+products = products.to_dict(orient='record')
 
-for wine in wines:
-    wine['title'] = wine['Название'] 
-    del wine['Название']
-    wine['grade'] = wine['Сорт']
-    del wine['Сорт']
-    wine['price'] = wine['Цена']
-    del wine['Цена']
-    wine['image'] = 'images/' + wine['Картинка']
-    del wine['Картинка']
+white_wines, drinks, red_wines, all_products = [], [], [], {}
+for product in products:
+    if product['Категория'] == 'Белые вина':
+        white_wines.append(product)
+    elif product['Категория'] == 'Напитки':
+        drinks.append(product)
+    else:
+        red_wines.append(product)       
+all_products['Белые вина'] = white_wines
+all_products['Напитки'] = drinks
+all_products['Красные вина'] = red_wines
+pprint(all_products)
 
-rendered_page = template.render(wines=wines, age = age_company, year_declension = year_declension(age_company) )
-print(wines)
+for produkt in products:
+    produkt['title'] = produkt['Название'] 
+    del produkt['Название']
+    produkt['grade'] = produkt['Сорт']
+    del produkt['Сорт']
+    produkt['price'] = produkt['Цена']
+    del produkt['Цена']
+    produkt['image'] = 'images/' + produkt['Картинка']
+    del produkt['Картинка']
+
+rendered_page = template.render(products=products, age = age_company, year_declension = year_declension(age_company) )
+
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8008), SimpleHTTPRequestHandler)
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
