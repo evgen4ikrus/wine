8560e25add96001151d2b5198ccc2baaa54d0cf8
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-05-20 05:38:59 +0300

    Revert "Удалил ненужный файл, высушил код"
    This reverts commit 8a6546ce709dc6802c1465f139c42a9b5bf24ea4.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import collections
+from collections import defaultdict
 import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
@@ -6,7 +6,19 @@ import isort
 import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-from functions import get_declension_year
+
+def get_company_age(company_foundation_year):
+    today = datetime.date.today()
+    company_age = today.year - company_foundation_year
+    if company_age % 10 == 1 and company_age % 100 != 11:
+        return f'{company_age} год'
+    elif company_age % 10 in [1, 2, 3, 4]:
+        if company_age % 100 // 10 == 1:
+            return f'{company_age} лет'
+        else:
+            return f'{company_age} года'
+    else:
+        return f'{company_age} лет'
 
 
 def main():
@@ -16,25 +28,13 @@ def main():
     )
     template = env.get_template('template.html')
     company_foundation_year = 1920
-    today = datetime.date.today()
-    company_age = today.year - company_foundation_year
-    company_age = str(company_age) + ' ' + get_declension_year(company_age)
-    
+    company_age = get_company_age(company_foundation_year) 
     excel_data_df = pandas.read_excel('table_sample.xlsx', sheet_name='Лист1', na_values='nan', keep_default_na=False)
     products = excel_data_df.to_dict(orient='record')
 
-    all_products = collections.defaultdict(list)
-
+    all_products = defaultdict(list)
     for product in products:
         all_products[product['Категория']].append(product)
-    
-    for product in products:
-        product['title'] = product.pop('Название')
-        product['grade'] = product.pop('Сорт')
-        product['price'] = product.pop('Цена')
-        product['image'] = 'images/' + product.pop('Картинка')
-        product['category'] = product.pop('Категория')
-        product['promotion'] = product.pop('Акция')
 
     rendered_page = template.render(all_products=all_products, company_age = company_age)
 
